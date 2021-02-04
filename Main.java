9cca89dd325298d3eef5eb10847a721952ef501d
maramibrahim04 <maram.ibrahim@patelhs.org>
2021-02-04 19:05:24 +0000

    A bunch of if/else statements doing the same thing The second method makes class schedule based on the grade level, their gpa, and if the user wants to take advanced classes or not

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner6;
 public class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
@@ -32,4 +34,50 @@ static int classAmount(int gradeLevel)
                 return 5;
   }
   static string scheduleGenerator(String name, int grade, double gpa, bool advance)
-} 
+{
+  String schedule = "";
+
+  // a series of if-else statements that will print out the student's schedule if(grade == 9)
+}
+         if(bool == true && gpa >= 3.5)
+         {
+                 schedule = "Algebra 1 Honors\nBiology Honors\nEnglish 1 Honors\nAP Human Geography\nAnd 3 electives of your choice"; 
+                 return schedule;
+         }
+         else if (bool == true && gpa < 3.5)
+         {
+                  schedule = "Sorry, but your gpa does not meet the requirment to take advace classes\n";
+                  schedule += "Algebra 1\nBiology\nEnglish 1\nU.S. Government\nAnd 3 electives of your choice";
+                  return schedule;
+         }
+         else
+         {
+                  schedule ="Algebra 1\nBiology\nEnglish 1\nU.S. Government\nAnd 3 electives of your choice";
+                  return schedule;
+         }
+         else if(grade == 10)
+         {
+                if(bool == true && gpa >= 3.5)
+                {
+                     schedule = "Geometry Honors\nAP Chemistry\nEnglish 2 Honors\nAnd 2 electives of your choice";
+                     return schedule;
+                }
+                else if(bool == true && gpa < 3.5)
+                {
+                      schedule = "Sorry, but your gpa does not meet the requirements\n";
+                      schedule += "Geometry\nChemistry\nEnglish 2\nU.S.History\nAnd 2 electives of your choice";
+                      return schedule;
+                }
+                else 
+                {
+                      schedule = "1\nBiology\nEnglish\nU.S. Government\nAnd 2 electives of your choice";
+                      return schedule;
+                }
+         }
+         else if(grade == 11)
+         {
+                  if(bool == true && gpa >= 3.5)
+                  {
+                    
+                  }
+         }
